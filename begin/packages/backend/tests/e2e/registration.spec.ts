790dcf8eb66a998e7716f0774564d49c4904b38b
Marc Kalil <marc@marckalil.com>
2025-06-12 12:18:46 +0200

    writing first test asserts

---
+++ begin/packages/backend/tests/e2e/registration.spec.ts
@@ -0,0 +1,100 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import path from "path";
+
+import { sharedTestRoot } from "@dddforum/shared/src/paths";
+
+const feature = loadFeature(
+  path.join(sharedTestRoot, "features/registration.feature")
+);
+
+defineFeature(feature, (test) => {
+  test("Successful registration with marketing emails accepted", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    let addEmailToMarketingList: any = {};
+    let createUserResponse: any = {};
+    let userInfo: any = {};
+    given("I am a new user", () => {});
+    when(
+      "I register with valid account details accepting marketing emails",
+      () => {}
+    );
+    then("I should be granted access to my account", () => {
+      expect(createUserResponse.status).toBe(201);
+      expect(createUserResponse.data).toBeDefined();
+      const { data } = createUserResponse;
+      expect(data).toHaveProperty("id");
+      expect(data).toHaveProperty("email", userInfo.email);
+      expect(data).toHaveProperty("firstName", userInfo.firstName);
+      expect(data).toHaveProperty("lastName", userInfo.lastName);
+      expect(data).toHaveProperty("username", userInfo.username);
+    });
+    and("I should expect to receive marketing emails", () => {
+      expect(addEmailToMarketingList.status).toBe(201);
+    });
+  });
+
+  test.skip("Successful registration without marketing emails accepted", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given("I am a new user", () => {});
+    when(
+      "I register with valid account details declining marketing emails",
+      () => {}
+    );
+    then("I should be granted access to my account", () => {});
+    and("I should not expect to receive marketing emails", () => {});
+  });
+
+  test.skip("Invalid or missing registration details", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given("I am a new user", () => {});
+    when("I register with invalid account details", () => {});
+    then(
+      "I should see an error notifying me that my input is invalid",
+      () => {}
+    );
+    and("I should not have been sent access to account details", () => {});
+  });
+
+  test.skip("Account already created with email", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given("a set of users already created accounts", (table) => {});
+    when("new users attempt to register with those emails", () => {});
+    then(
+      "they should see an error notifying them that the account already exists",
+      () => {}
+    );
+    and("they should not have been sent access to account details", () => {});
+  });
+
+  test.skip("Username already taken", ({ given, when, then, and }) => {
+    given(
+      "a set of users have already created their accounts with valid details",
+      (table) => {}
+    );
+    when(
+      "new users attempt to register with already taken usernames",
+      (table) => {}
+    );
+    then(
+      "they see an error notifying them that the username has already been taken",
+      () => {}
+    );
+    and("they should not have been sent access to account details", () => {});
+  });
+});
