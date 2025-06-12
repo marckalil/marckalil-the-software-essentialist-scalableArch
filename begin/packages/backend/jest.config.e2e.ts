6c8602bcd0e680c42ba993ebaf4787e10f121686
Marc Kalil <marc@marckalil.com>
2025-06-12 15:23:13 +0200

    Revert "add jest setup"
    This reverts commit db49afa8b4c9816ff547bf38a4cd6496d7fbcff1.

--- begin/packages/backend/jest.config.e2e.ts
+++ begin/packages/backend/jest.config.e2e.ts
@@ -18,7 +18,6 @@ const config: Config.InitialOptions = {
       tsconfig: "tsconfig.json",
     },
   },
-  setupFilesAfterEnv: ["<rootDir>/tests/.jestSetup.ts"],
   verbose: true,
   detectOpenHandles: true,
   forceExit: true,
