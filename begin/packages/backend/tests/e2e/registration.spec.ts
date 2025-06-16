3efba656b99b4849c7327a0206ee2a04329e40ec
Marc Kalil <marc@marckalil.com>
2025-06-16 17:41:55 +0200

    clarify 'Account already created with email' scenario test implementation

--- begin/packages/backend/tests/e2e/registration.spec.ts
+++ begin/packages/backend/tests/e2e/registration.spec.ts
@@ -139,10 +139,10 @@ defineFeature(feature, (test) => {
   });
 
   test("Account already created with email", ({ given, when, then, and }) => {
-    let existingUsers: CreateUserInput[] = [];
+    let newUsers: CreateUserInput[] = [];
     let createUserResponses: any[] = [];
     given("a set of users already created accounts", async (table) => {
-      existingUsers = table.map((row: any) =>
+      const existingUsers = table.map((row: any) =>
         new CreateUserInputBuilder()
           .withAllRandomDetails()
           .withEmail(row.email)
@@ -151,12 +151,16 @@ defineFeature(feature, (test) => {
           .build()
       );
       await databaseFixtures.setUpWithExistingUsers(existingUsers);
+      newUsers = existingUsers.map((user: CreateUserInput) => {
+        return new CreateUserInputBuilder()
+          .withAllRandomDetails()
+          .withEmail(user.email)
+          .build();
+      });
     });
     when("new users attempt to register with those emails", async () => {
       createUserResponses = await Promise.all(
-        existingUsers.map((input) =>
-          request(app).post("/users/new").send(input)
-        )
+        newUsers.map((input) => request(app).post("/users/new").send(input))
       );
     });
     then(
