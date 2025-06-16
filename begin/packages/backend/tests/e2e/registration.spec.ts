d18629242f4e9e4c23253bb5db7a091c55ffbed3
Marc Kalil <marc@marckalil.com>
2025-06-16 17:00:44 +0200

    characterize 'Invalid or missing registration details' scenario

--- begin/packages/backend/tests/e2e/registration.spec.ts
+++ begin/packages/backend/tests/e2e/registration.spec.ts
@@ -65,34 +65,76 @@ defineFeature(feature, (test) => {
     });
   });
 
-  test.skip("Successful registration without marketing emails accepted", ({
+  test("Successful registration without marketing emails accepted", ({
     given,
     when,
     then,
     and,
   }) => {
-    given("I am a new user", () => {});
+    let addEmailToMarketingList: any = {};
+    let createUserResponse: any = {};
+    let createUserInput: CreateUserInput;
+    given("I am a new user", async () => {
+      createUserInput = new CreateUserInputBuilder()
+        .withAllRandomDetails()
+        .withEmail("x@y.com")
+        .withFirstName("John")
+        .withLastName("Doe")
+        .withUsername("johndoe")
+        .build();
+    });
     when(
       "I register with valid account details declining marketing emails",
-      () => {}
+      async () => {
+        createUserResponse = await request(app)
+          .post("/users/new")
+          .send(createUserInput);
+      }
     );
-    then("I should be granted access to my account", () => {});
-    and("I should not expect to receive marketing emails", () => {});
+    then("I should be granted access to my account", () => {
+      expect(createUserResponse.status).toBe(201);
+      const { data, error, success } = createUserResponse.body;
+      expect(success).toBeTruthy();
+      expect(error).toBeUndefined();
+      expect(data).toBeDefined();
+      expect(data).toHaveProperty("id");
+      expect(data).toHaveProperty("email", createUserInput.email);
+      expect(data).toHaveProperty("firstName", createUserInput.firstName);
+      expect(data).toHaveProperty("lastName", createUserInput.lastName);
+      expect(data).toHaveProperty("username", createUserInput.username);
+    });
+    and("I should not expect to receive marketing emails", () => {
+      // To fill in later
+    });
   });
 
-  test.skip("Invalid or missing registration details", ({
+  test("Invalid or missing registration details", ({
     given,
     when,
     then,
     and,
   }) => {
-    given("I am a new user", () => {});
-    when("I register with invalid account details", () => {});
-    then(
-      "I should see an error notifying me that my input is invalid",
-      () => {}
-    );
-    and("I should not have been sent access to account details", () => {});
+    let inValidUserInput: Partial<CreateUserInput> = {};
+    let response: any = {};
+    given("I am a new user", () => {
+      const createUserInput =
+        new CreateUserInputBuilder().withAllRandomDetails();
+      inValidUserInput = {
+        ...createUserInput,
+        email: undefined,
+      };
+    });
+    when("I register with invalid account details", async () => {
+      response = await request(app).post("/users/new").send(inValidUserInput);
+    });
+    then("I should see an error notifying me that my input is invalid", () => {
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty("success", false);
+      expect(response.body).toHaveProperty("error", "ValidationError");
+    });
+    and("I should not have been sent access to account details", () => {
+      expect(response.body).not.toHaveProperty("data");
+    });
   });
 
   test.skip("Account already created with email", ({
