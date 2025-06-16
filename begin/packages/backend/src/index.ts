177acde941f758e4cae6b70f6f2284cdeca10ebf
Marc Kalil <marc@marckalil.com>
2025-06-16 18:19:43 +0200

    create users controller

--- begin/packages/backend/src/index.ts
+++ begin/packages/backend/src/index.ts
@@ -5,140 +5,14 @@ import { MarketingController } from "./modules/marketing";
 import { MarketingService } from "./modules/marketing/marketingService";
 import { ContactListAPI } from "./modules/marketing/contactListApi";
 import { marketingErrorHandler } from "./modules/marketing/marketingErrors";
+import { UsersController } from "./modules/users/usersController";
 const cors = require("cors");
+
+import { Errors } from "./shared/errors";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const Errors = {
-  UsernameAlreadyTaken: "UserNameAlreadyTaken",
-  EmailAlreadyInUse: "EmailAlreadyInUse",
-  ValidationError: "ValidationError",
-  ServerError: "ServerError",
-  ClientError: "ClientError",
-  UserNotFound: "UserNotFound",
-};
-
-function isMissingKeys(data: any, keysToCheckFor: string[]) {
-  for (let key of keysToCheckFor) {
-    if (data[key] === undefined) return true;
-  }
-  return false;
-}
-
-function generateRandomPassword(length: number): string {
-  const charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
-  const passwordArray = [];
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    passwordArray.push(charset[randomIndex]);
-  }
-
-  return passwordArray.join("");
-}
-
-function parseUserForResponse(user: User) {
-  const returnData = JSON.parse(JSON.stringify(user));
-  delete returnData.password;
-  return returnData;
-}
-
-// Create a new user
-app.post("/users/new", async (req: Request, res: Response) => {
-  try {
-    const keyIsMissing = isMissingKeys(req.body, [
-      "email",
-      "firstName",
-      "lastName",
-      "username",
-    ]);
-
-    if (keyIsMissing) {
-      return res.status(400).json({
-        error: Errors.ValidationError,
-        data: undefined,
-        success: false,
-      });
-    }
-
-    const userData = req.body;
-
-    const existingUserByEmail = await prisma.user.findFirst({
-      where: { email: req.body.email },
-    });
-    if (existingUserByEmail) {
-      return res.status(409).json({
-        error: Errors.EmailAlreadyInUse,
-        data: undefined,
-        success: false,
-      });
-    }
-
-    const existingUserByUsername = await prisma.user.findFirst({
-      where: { username: req.body.username as string },
-    });
-    if (existingUserByUsername) {
-      return res.status(409).json({
-        error: Errors.UsernameAlreadyTaken,
-        data: undefined,
-        success: false,
-      });
-    }
-
-    const { user, member } = await prisma.$transaction(async (tx) => {
-      const user = await prisma.user.create({
-        data: { ...userData, password: generateRandomPassword(10) },
-      });
-      const member = await prisma.member.create({ data: { userId: user.id } });
-      return { user, member };
-    });
-
-    return res.status(201).json({
-      error: undefined,
-      data: parseUserForResponse(user),
-      success: true,
-    });
-  } catch (error) {
-    console.log(error);
-    // Return a failure error response
-    return res
-      .status(500)
-      .json({ error: Errors.ServerError, data: undefined, success: false });
-  }
-});
-
-// Get a user by email
-app.get("/users", async (req: Request, res: Response) => {
-  try {
-    const email = req.query.email as string;
-    if (email === undefined) {
-      return res.status(400).json({
-        error: Errors.ValidationError,
-        data: undefined,
-        success: false,
-      });
-    }
-
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      return res
-        .status(404)
-        .json({ error: Errors.UserNotFound, data: undefined, success: false });
-    }
-
-    return res.status(200).json({
-      error: undefined,
-      data: parseUserForResponse(user),
-      succes: true,
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ error: Errors.ServerError, data: undefined, success: false });
-  }
-});
 
 // Get posts
 app.get("/posts", async (req: Request, res: Response) => {
@@ -185,6 +59,10 @@ const marketingController = new MarketingController(
   marketingService,
   marketingErrorHandler
 );
+
+const usersController = new UsersController();
+
+app.use("/users", usersController.getRouter());
 app.use("/marketing", marketingController.getRouter());
 
 app.listen(port, () => {
--- begin/packages/backend/src/shared/errors/index.ts
+++ begin/packages/backend/src/shared/errors/index.ts
@@ -1,6 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
 import { CustomException } from "../exceptions";
 
+export const Errors = {
+  UsernameAlreadyTaken: "UserNameAlreadyTaken",
+  EmailAlreadyInUse: "EmailAlreadyInUse",
+  ValidationError: "ValidationError",
+  ServerError: "ServerError",
+  ClientError: "ClientError",
+  UserNotFound: "UserNotFound",
+};
+
 export type ErrorHandler = (
   error: CustomException,
   req: Request,
