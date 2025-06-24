8676f798b9d956c5d4d460a4be346981f0ec0f1d
Marc Kalil <marc@marckalil.com>
2025-06-24 17:18:58 +0200

    clean error types

--- begin/packages/backend/src/shared/errors/index.ts
+++ begin/packages/backend/src/shared/errors/index.ts
@@ -1,15 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { CustomException } from "../exceptions";
 
-export const Errors = {
-  UsernameAlreadyTaken: "UsernameAlreadyTaken",
-  EmailAlreadyInUse: "EmailAlreadyInUse",
-  ValidationError: "ValidationError",
-  ServerError: "ServerError",
-  ClientError: "ClientError",
-  UserNotFound: "UserNotFound",
-};
-
 export type ErrorHandler = (
   error: CustomException,
   req: Request,
