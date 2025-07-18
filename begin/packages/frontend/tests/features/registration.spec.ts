7737d719b66dc37e6b9021879cd8c2d3e907ea9e
Marc Kalil <marc@marckalil.com>
2025-07-18 17:24:56 +0200

    create success scenario skeleton

--- begin/packages/frontend/tests/features/registration.spec.ts
+++ begin/packages/frontend/tests/features/registration.spec.ts
@@ -1,6 +1,10 @@
 import { defineFeature, loadFeature } from "jest-cucumber";
-import { sharedTestRoot } from "@dddforum/shared/src/paths";
 import * as path from "path";
+
+import { CreateUserInput } from "@dddforum/shared/src/api/users";
+import { CreateUserInputBuilder } from "@dddforum/shared/tests/support/builders/CreateUserInputBuilder";
+import { DatabaseFixtures } from "@dddforum/shared/tests/support/fixtures/databaseFixtures";
+import { sharedTestRoot } from "@dddforum/shared/src/paths";
 
 const feature = loadFeature(
   path.join(sharedTestRoot, "features/registration.feature"),
@@ -8,18 +12,55 @@ const feature = loadFeature(
 );
 
 defineFeature(feature, (test) => {
+  let userInput: CreateUserInput;
+  let databaseFixtures: DatabaseFixtures;
+  let app: App;
+  let pages: Pages;
+  let layout: Layout;
+  let puppeteerPageDriver: PuppeteerPageDriver;
+
+  beforeAll(async () => {
+    databaseFixtures = new DatabaseFixtures();
+    puppeteerPageDriver = await PuppeteerPageDriver.create({
+      headless: false,
+      slowMo: 50,
+    });
+    app = createAppObject(puppeteerPageDriver);
+    pages = app.pages();
+    layout = app.layout();
+  });
+
+  beforeEach(async () => {
+    await databaseFixtures.reset();
+  });
+
+  afterAll(async () => {
+    await puppeteerPageDriver.browser.close();
+  });
+
   test("Successful registration with marketing emails accepted", ({
     given,
     when,
     then,
     and,
   }) => {
-    given("I am a new user", () => {});
+    given("I am a new user", () => {
+      userInput = new CreateUserInputBuilder().withAllRandomDetails().build();
+    });
     when(
       "I register with valid account details accepting marketing emails",
-      async () => {}
+      async () => {
+        await pages.registration.open();
+        await pages.registration.enterFormDetails(userInput);
+        await pages.registration.acceptMarketingEmails();
+        await pages.registration.submitForm();
+      }
     );
-    then("I should be granted access to my account", async () => {});
+    then("I should be granted access to my account", async () => {
+      const username = await layout.header.getLoggedInUserName();
+      expect(username).toBeDefined();
+      expect(username).toContain(userInput.username);
+    });
     and("I should expect to receive marketing emails", () => {
       // @See backend
     });
